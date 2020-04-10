7ede9dc56e4b9f98845d45570859813181988545
JorgeDLG <idroj07@gmail.com>
2020-04-10 13:38:04 +0200

    Creando clase Users para SeedData

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClaveSol.Models
+{
+    public class User
+    {
+        public int Id {get; set;}
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,9 +29,13 @@ namespace ClaveSol
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
            services.AddRazorPages();
         }
