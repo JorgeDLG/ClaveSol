d0018ca674d46f29e6ee00900aa165240292a858
JorgeDLG <idroj07@gmail.com>
2020-04-23 12:26:41 +0200

    user model & appDbContext created
    appdbcontexdt created

--- Models/User.cs
+++ Models/User.cs
@@ -6,11 +6,25 @@ namespace ClaveSol.Models
     [Table("User")]
     public class User
     {
-        [Column("UserID")]
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required(ErrorMessage = "User ID is required")]
         [Display(Name = "User ID")]
         public int Id { get; set; }
+
+        [StringLength(30,MinimumLength = 2)]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name {get; set;}
+
+        [StringLength(60,MinimumLength = 2)]
+        [Required(ErrorMessage = "Surname is required")]
+        public string Surname {get; set;}
+
+        [EmailAddress]
+        [Required]
+        public string Mail {get; set;}
+
+        [Required]
+        public bool Premium {get; set;}
     }
 }
