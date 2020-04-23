93417700a89af31a87203e4a3e76e86d78cb11d0
JorgeDLG <idroj07@gmail.com>
2020-04-23 20:21:41 +0200

    init Migration of AppDb User table created

---
+++ Migrations/20200423182114_initApp-User.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClaveSol.Migrations
+{
+    public partial class initAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Surname = table.Column<string>(maxLength: 60, nullable: false),
+                    Mail = table.Column<string>(nullable: false),
+                    Premium = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
