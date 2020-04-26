b565d2defd7e3579c8af982af14a1950477c6b68
JorgeDLG <idroj07@gmail.com>
2020-04-27 00:38:54 +0200

    Working on UserIsOwnerAuthorizationHandler (Auth MS Tuto)

---
+++ Security/UserIsOwnerAuthorizationHandler.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualBasic;
+using ClaveSol.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace ClaveSol.Security
+{
+    public class UserIsOwnerAuthorizationHandler 
+        : AuthorizationHandler<OperationAuthorizationRequirement, User>
+    {
+        UserManager<IdentityUser> _userManager;
+
+        public UserIsOwnerAuthorizationHandler(UserManager<appIdentityUser> 
+            userManager)
+        {
+            _userManager = userManager;
+        }
+
+        protected override Task
+            HandleRequirementAsync(AuthorizationHandlerContext context,
+                                   OperationAuthorizationRequirement requirement,
+                                   User resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            // If not asking for CRUD permission, return.
+
+            //% CONSTANTS CUSTOM CLASS CREATED here: https://docs.microsoft.com/es-es/aspnet/core/security/authorization/secure-data?view=aspnetcore-3.1#review-the-contact-operations-requirements-class
+
+            if (requirement.Name != Constants.CreateOperationName &&
+                requirement.Name != Constants.ReadOperationName   &&
+                requirement.Name != Constants.UpdateOperationName &&
+                requirement.Name != Constants.DeleteOperationName )
+            {
+                return Task.CompletedTask;
+            }
+
+            if (resource.OwnerID == _userManager.GetUserId(context.User))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
