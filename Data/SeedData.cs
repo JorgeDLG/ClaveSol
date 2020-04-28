49ea5b6faabe1464a7c7c5ccc6f8de8244fa6c40
JorgeDLG <idroj07@gmail.com>
2020-04-28 12:23:47 +0200

    SeedData mod , db's Wiped

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,114 +1,161 @@
+using System.ComponentModel;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ClaveSol.Models;
 using System;
+using System.Collections;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ClaveSol.Security;
 
 namespace ClaveSol.Data
-{
+{//UPDATE: Seed Users(ClaveSolDbContext) & Create Identity Users and Roles LINKING with it.
     public static class SeedData
     {
-        public static void Initialize(IServiceProvider serviceProvider)
+        public static async Task Initialize(IServiceProvider serviceProvider, string testUserPw)
         {
-            using (var context = new ClaveSolDbContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<ClaveSolDbContext>>()))
+            // For sample purposes seed both with the same password.
+            // Password is set with the following:
+            // dotnet user-secrets set SeedUserPW <pw>
+            // The admin user can do anything
+
+            //Identity following this tutorial: https://bit.ly/3cKxRXz
+
+            string[,] adminUsers = new string[1, 4];
+            var admin = await EnsureUser(serviceProvider, testUserPw, "admin@mail.com");
+            await EnsureRole(serviceProvider, admin.Id, "admin"/*Constants.ContactAdministratorsRole*/);
+
+            var context = new ClaveSolDbContext(serviceProvider.
+            GetRequiredService<DbContextOptions<ClaveSolDbContext>>());
+
+            adminUsers[0, 0] = "admin";
+            adminUsers[0, 1] = admin.UserName;//mail
+            adminUsers[0, 2] = "False";
+            adminUsers[0, 3] = admin.Id;
+
+            SeedAppDB(context, adminUsers);
+
+            //Loop 4 NormalUsers(Identity) generation,role addition & linked/generated to AppUsers. 
+            string[] userNamesSeed = { "ana", "paco", "mario", "arturo" };
+            string[,] normalUsers = new string[userNamesSeed.Length,4];
+            for (int i = 0; i < userNamesSeed.Length; i++)
             {
-                // Look for any movies.
-                if (context.User.Any())
-                {
-                    return;   // DB has been seeded
-                }
+                var normal = await EnsureUser(serviceProvider,
+                     testUserPw, $"{userNamesSeed[i]}@mail.com");
+                await EnsureRole(serviceProvider, normal.Id, "normal"/*Constants.ContactManagersRole*/);
 
-                context.User.AddRange(
-                    new User
-                    {
-                        Name = "Ana",
-                        Surname = "Perez",
-                        Mail = "ana@mail.com",
-                        Premium = false,
+                normalUsers[i, 0] = userNamesSeed[i];
+                normalUsers[i, 1] = $"{userNamesSeed[i]}@mail.com";
+                normalUsers[i, 2] = "False";
+                normalUsers[i, 3] = normal.Id;
+            }
+            SeedAppDB(context, normalUsers);
+        }
+        private static async Task<AppIdentityUser> EnsureUser(IServiceProvider serviceProvider,
+                                            string testUserPw, string UserName)
+        {
+            var userManager = serviceProvider.GetService<UserManager<AppIdentityUser>>();
 
-                        //NOTA El ID en IdentityDB es un Hash! OwnerID = mail?
-
-                        OwnerID = "1"
-                    },
-                    new User
-                    {
-                        Name = "Paco",
-                        Surname = "Perez",
-                        Mail = "paco@mail.com",
-                        Premium = false,
-                        OwnerID = "2"
-                    },
-                    new User
-                    {
-                        Name = "Mario",
-                        Surname = "Garcia",
-                        Mail = "mario@mail.com",
-                        Premium = true,
-                        OwnerID = "3"
-                    },
-                    new User
-                    {
-                        Name = "Arturo",
-                        Surname = "Sanchez",
-                        Mail = "arturo@mail.com",
-                        Premium = true,
-                        OwnerID = "4"
-                    }
-                );
-                context.SaveChanges();
+            AppIdentityUser user = null;
+            try
+            {
+                user = await userManager.FindByNameAsync(UserName);
+            }
+            catch (System.Exception)
+            {
+                throw;
             }
 
-            /*using (var context = new ApplicationDbContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<ApplicationDbContext>>()))
+            if (user == null)
             {
-                // Look for any movies.
-                if (context.AspNetUsers.Any())
+                user = new AppIdentityUser
+                {
+                    UserName = UserName,
+                    EmailConfirmed = true
+                    //FullName ?
+                };
+                await userManager.CreateAsync(user, testUserPw);
+            }
+
+            if (user == null)
+            {
+                throw new Exception("The password is probably not strong enough!");
+            }
+            return user;
+        }
+
+        private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider,
+                                                                      string uid, string role)
+        {
+            IdentityResult IR = null;
+            var roleManager = serviceProvider.GetService<RoleManager<AppIdentityRole>>();
+
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager null");
+            }
+
+            try
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    IR = await roleManager.CreateAsync(new AppIdentityRole(role));
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+
+            var userManager = serviceProvider.GetService<UserManager<AppIdentityUser>>();
+
+            var user = await userManager.FindByIdAsync(uid);
+
+            if (user == null)
+            {
+                throw new Exception("The testUserPw password was probably not strong enough!");
+            }
+
+            IR = await userManager.AddToRoleAsync(user, role);
+
+            return IR;
+        }
+        public static void SeedAppDB(ClaveSolDbContext context, string[,] IdentityUsers)
+        {
+            if (context.User.Any())
+            {
+                if (IdentityUsers[0,0] != "ana" && context.User.Count() != 1 || context.User.Count() >= 5) // last || prescindible?
                 {
                     return;   // DB has been seeded
                 }
+            }
 
-                context.User.AddRange(
-                    new User
-                    {
-                        Name = "Ana",
-                        Surname = "Perez",
-                        Mail = "ana@mail.com",
-                        Premium = false,
-                        OwnerID = "1"
-                    },
-                    new User
-                    {
-                        Name = "Paco",
-                        Surname = "Perez",
-                        Mail = "paco@mail.com",
-                        Premium = false,
-                        OwnerID = "2"
-                    },
-                    new User
-                    {
-                        Name = "Mario",
-                        Surname = "Garcia",
-                        Mail = "mario@mail.com",
-                        Premium = true,
-                        OwnerID = "3"
-                    },
-                    new User
-                    {
-                        Name = "Arturo",
-                        Surname = "Sanchez",
-                        Mail = "arturo@mail.com",
-                        Premium = true,
-                        OwnerID = "4"
-                    }
-                );
-                context.SaveChanges();
-            }*/
+            try
+            {
+                for (int i = 0; i < IdentityUsers.GetLength(0); i++)
+                {
+                    context.User.Add(
+                        new User
+                        {
+                            Name = IdentityUsers[i, 0],
+                            Surname = "Perez",
+                            Mail = IdentityUsers[i, 1],
+                            Premium = Convert.ToBoolean(IdentityUsers[i, 2]),
+                            OwnerID = IdentityUsers[i, 3]
+                        }
+                    );
+                }
+
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+
+            context.SaveChanges();
         }
     }
 
 }
-
-
