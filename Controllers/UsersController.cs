5682d80adc42f229891737ec275e0271d2f17595
JorgeDLG <idroj07@gmail.com>
2020-05-08 23:55:28 +0200

    Users/{Edit,Delete} link w/ Identity,Createds NO LOGIN
    Avoid create users form admin control? EmailConfirmed property still
    false, try to fix?

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ClaveSol.Data;
 using ClaveSol.Models;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
 
 namespace ClaveSol.Controllers
 {
@@ -69,7 +71,7 @@ namespace ClaveSol.Controllers
             {
                 _context.Add(user);
 
-                IdentityLink( pass, role, user,'c');
+                await IdentityLink( pass, role, user,'c');
 
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -98,8 +100,9 @@ namespace ClaveSol.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,Mail,Premium,OwnerID,Status")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Surname,Mail,Premium,OwnerID,Status")] User user, string pass, string role)
         {
+            user.Status = 0;
             if (id != user.Id)
             {
                 return NotFound();
@@ -110,6 +113,7 @@ namespace ClaveSol.Controllers
                 try
                 {
                     _context.Update(user);
+                    await IdentityLink( null, role, user,'e');
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -153,6 +157,8 @@ namespace ClaveSol.Controllers
         {
             var user = await _context.User.FindAsync(id);
             _context.User.Remove(user);
+
+            await IdentityLink( null, null, user,'d');
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -161,7 +167,7 @@ namespace ClaveSol.Controllers
         {
             return _context.User.Any(e => e.Id == id);
         }
-        public void IdentityLink(string pass,string role,User user,char operation)
+        public async Task IdentityLink(string pass,string role,User user,char operation)
         {
             //IdentityUser idUser;
 
@@ -169,16 +175,47 @@ namespace ClaveSol.Controllers
             {
                 case 'c': //Create
                     var idUser = new IdentityUser { UserName = user.Mail, Email = user.Mail };
-                var result = _userManager.CreateAsync(idUser, pass);
-                var result2 = _userManager.AddToRoleAsync(idUser, role);
-                user.OwnerID = idUser.Id;
+                    var result = await _userManager.CreateAsync(idUser, pass);
+                    var result2 = _userManager.AddToRoleAsync(idUser, role);
+                    user.OwnerID = idUser.Id;
+
+                    //let login
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(idUser);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = idUser.Id, code = code },
+                        protocol: Request.Scheme);
+                    idUser.EmailConfirmed = true;
 
                 break;
+
                 case 'e': //Edit
+                    var idUser2 = await _userManager.FindByIdAsync(user.OwnerID);
+                    bool isAdmin = await _userManager.IsInRoleAsync(idUser2,"admin");
+
+                    if (isAdmin && role == "normal")
+                    {
+                        await _userManager.RemoveFromRoleAsync(idUser2,"admin");
+                        await _userManager.AddToRoleAsync(idUser2,"normal");
+                    }
+                    else if(!isAdmin && role == "admin")
+                    {
+                        await _userManager.RemoveFromRoleAsync(idUser2,"normal");
+                        await _userManager.AddToRoleAsync(idUser2,"admin");
+                    }
+                    idUser2.Email = user.Mail;
+                    idUser2.UserName = user.Mail;
+
+                    idUser2.EmailConfirmed = true;
+                    await _userManager.UpdateAsync(idUser2);
                 break;
+
                 case 'd': //Delete
+                    var idUser3 = await _userManager.FindByIdAsync(user.OwnerID);
+                    var res = await _userManager.DeleteAsync(idUser3);
                 break;
-                
                 default:
                     Console.WriteLine("Unexpeted operation in switch case");
                 break;
