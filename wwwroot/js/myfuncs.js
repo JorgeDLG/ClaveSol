b1a0a618794aa76a111f42d5beee21ef1b5d974c
JorgeDLG <idroj07@gmail.com>
2020-05-13 18:03:26 +0200

    basic darkMode working

---
+++ wwwroot/js/myfuncs.js
@@ -0,0 +1,24 @@
+console.log("loaded");
+
+var elements = $('body,nav,p,svg').not('.bi-bucket');
+
+if (localStorage.getItem('darkMode') == null) {
+    localStorage.setItem('darkMode','0');
+}
+
+var inDarkMode = localStorage.getItem('darkMode');   
+
+if (inDarkMode == 1) {
+    elements.addClass("bg-dark text-white")
+}
+
+function switchDarkMode() {
+    inDarkMode = localStorage.getItem('darkMode');
+    elements.toggleClass("bg-dark text-white");
+
+    if (inDarkMode == '0') {
+        localStorage.setItem('darkMode','1'); //NO CAMBIA
+    }else{
+        localStorage.setItem('darkMode','0');
+    } 
+}
