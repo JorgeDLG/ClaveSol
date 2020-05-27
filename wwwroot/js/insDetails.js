fd3a40dadd3c7e25caeef9d851c6bc50075e40c3
JorgeDLG <idroj07@gmail.com>
2020-05-27 23:25:37 +0200

    js color selection OK

--- wwwroot/js/insDetails.js
+++ wwwroot/js/insDetails.js
@@ -1,3 +1,5 @@
+////////////////// MEDIA ////////////////////
+
 var escenario = $("#escenario");
 var plano = $("#plano");
 var plano2 = $("#2plano");
@@ -5,7 +7,7 @@ var plano3 = $("#3plano");
 var plano4 = $("#4plano");
 
 
-var mediaEsc = $(".multimedia").find("#escenario"); 
+var mediaEsc = $(".multimedia").find("#escenario");
 
 //escenario.hover(addControls);
 ClickEvToPlanos();
@@ -17,21 +19,21 @@ function swap(plano) {
         4. Si han perdido atributos añadelos. 
             (IF video esta en escenario add controls)
     */
-   //1 
-   
-   var mediaPlano = $(".multimedia").find("#"+plano); 
-   //mediaPlano.css("border","1px solid red");
+    //1 
+
+    var mediaPlano = $(".multimedia").find("#" + plano);
+    //mediaPlano.css("border","1px solid red");
 
     var objPlano = $(mediaPlano).find("video");
     if (objPlano.length == 0) {
-       objPlano =  $(mediaPlano).find("img");
-       objPlano.length == 0 ? console.log("Plano no contiene img ni vid") : console.log("Plano contiene img") 
+        objPlano = $(mediaPlano).find("img");
+        objPlano.length == 0 ? console.log("Plano no contiene img ni vid") : console.log("Plano contiene img")
     }
 
     var objEsc = $(mediaEsc).find("video");
     if (objEsc.length == 0) {
-       objEsc =  $(mediaEsc).find("img");
-       objEsc.length == 0 ? console.log("Escenario no contiene img ni vid") : console.log("Escenario contiene img") 
+        objEsc = $(mediaEsc).find("img");
+        objEsc.length == 0 ? console.log("Escenario no contiene img ni vid") : console.log("Escenario contiene img")
     }
 
     objEsc.replaceWith(objPlano);
@@ -40,22 +42,45 @@ function swap(plano) {
 }
 
 function addControls() {
-//AMPLIAR ELEMENTO + boton escape
-   mediaEsc.css("border","1px solid red");
+    //AMPLIAR ELEMENTO + boton escape
+    mediaEsc.css("border", "1px solid red");
     console.log(mediaEsc);
 
     video = mediaEsc.find("video");
-    if ( video.length == 1) {
-       mediaEsc.css("border","1px solid blue");
-       video.prop("controls"); 
+    if (video.length == 1) {
+        mediaEsc.css("border", "1px solid blue");
+        video.prop("controls");
     }
 }
 
 function ClickEvToPlanos() {
     for (let i = 1; i <= 4; i++) {
         let plaNo, tagPlano;
-        i == 1 ? (plaNo="plano",tagPlano="plano") : (plaNo="plano"+i, tagPlano=i+"plano")  
-    
+        i == 1 ? (plaNo = "plano", tagPlano = "plano") : (plaNo = "plano" + i, tagPlano = i + "plano")
+
         eval(plaNo).on("click", () => swap(tagPlano));
     }
+}
+
+////////////////// ATTRIBUTES ////////////////////
+
+var colorsCont = $("#Color");
+
+try {
+    var colorsOptions = colorsCont.find("div");
+
+    colorsOptions.each(function () {
+        $(this).click(select);
+    });
+
+} catch (error) {
+    console.log(error);
+}
+
+function select() {
+    colorsCont.find("input").removeAttr("checked");
+    colorsCont.find("input").removeClass("selected");
+
+    $(this).addClass("selected");
+    $(this).find("input").attr("checked", "checked");
 }
