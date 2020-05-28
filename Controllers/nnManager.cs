d0b9968ff483ccc62dedb2e5697e2dd60df723af
JorgeDLG <idroj07@gmail.com>
2020-05-28 14:43:59 +0200

    Err with nnModel model passed to view(+) InvalidOperationException: The model item passed into the ViewDataDictionary is of type 'ClaveSol.Models.nnModels', but this ViewDataDictionary instance requires a model item of type 'System.Collections.Generic.IEnumerable`1[ClaveSol.Models.nnModels]'.

---
+++ Controllers/nnManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ClaveSol.Data;
+using ClaveSol.Models;
+
+namespace ClaveSol.Controllers
+{
+    public class nnManager : Controller
+    {
+        private readonly ClaveSolDbContext  _context;
+
+        public nnManager(ClaveSolDbContext  context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var claveSolDbContext = _context.Comment.Include(c => c.Instrument).Include(c => c.User);
+
+            var nnModels = new nnModels
+            {
+                AttrIns = await _context.Attribut_Ins.ToListAsync(), 
+                ListIns = await _context.List_Instrument.ToListAsync(), 
+                ShopIns = await _context.Shop_Ins.ToListAsync()
+            };
+            return View(nnModels);
+        }
+
+    }
+}
