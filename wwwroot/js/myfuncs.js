e7dec4e2bce63753a58cf9706bb83534a1cb93da
JorgeDLG <idroj07@gmail.com>
2020-05-30 17:30:11 +0200

    css work (dark theme fixes, responsive tables,etc)

--- wwwroot/js/myfuncs.js
+++ wwwroot/js/myfuncs.js
@@ -1,23 +1,26 @@
-console.log("loaded");
+console.log("loaded myfuncs.js");
 
-var elements = $('body,nav,p,svg,.card-body').not('.bi-bucket');
+var elements = $('body,nav,p,svg,.card-body,ol,div.card,li').not('.bi-bucket');
+var tables = $('table');
 
-if (localStorage.getItem('darkMode') == null) {
+if (localStorage.getItem('darkMode') == null) { //init rutine (inizialice darkMode to false)
     localStorage.setItem('darkMode','0');
 }
 
-var inDarkMode = localStorage.getItem('darkMode');   
+var inDarkMode = localStorage.getItem('darkMode');  //retrieve darkmode variable 
 
 if (inDarkMode == 1) {
     elements.addClass("bg-dark text-white")
+    tables.addClass("table-dark")
 }
 
-function switchDarkMode() {
+function switchDarkMode() { //User input Event
     inDarkMode = localStorage.getItem('darkMode');
     elements.toggleClass("bg-dark text-white");
+    tables.toggleClass("table-dark");
 
-    if (inDarkMode == '0') {
-        localStorage.setItem('darkMode','1'); //NO CAMBIA
+    if (inDarkMode == '0') { //Variable switcher (update state)
+        localStorage.setItem('darkMode','1');
     }else{
         localStorage.setItem('darkMode','0');
     } 
