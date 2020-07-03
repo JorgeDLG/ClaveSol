9503db8674948d6ed6e9d183d161ae0325432839
JorgeDLG <idroj07@gmail.com>
2020-07-03 10:19:10 +0200

    Release version

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using ClaveSol.Models;
 
 namespace ClaveSol.Controllers
 {
+    [Authorize]
     public class OrdersController : Controller
     {
         private readonly ClaveSolDbContext _context;
